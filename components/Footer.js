cd9e6046dd148a81446e8d5eb29865e748201660
afifdev <afifudin.fdn@gmail.com>
2021-11-11 20:42:44 +0700

    Adding Footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="fixed w-screen bottom-0 left-0 bg-black text-white">
+      <div className="max-w-7x m-auto px-8 py-1 text-center">
+        Haven't Login Yet? <span className="font-bold text-red-600">Login</span>
+      </div>
+    </div>
+  );
+}
