2c98ba1be2c965d39be2c0014b31611f50fee6af
afifdev <afifudin.fdn@gmail.com>
2021-11-12 22:29:15 +0700

    Use Link in Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 export default function Footer() {
   return (
     <div className="fixed w-screen bottom-0 left-0 bg-black text-white">
       <div className="max-w-7x m-auto px-8 py-1 text-center">
-        Haven't Login Yet? <span className="font-bold text-red-600">Login</span>
+        Haven't Login Yet?{" "}
+        <Link href="/login">
+          <a className="font-bold text-red-600">Login</a>
+        </Link>
       </div>
     </div>
   );
