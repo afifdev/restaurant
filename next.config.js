fc30f86439486163e1f27b830d5168b019c09f97
afifdev <afifudin.fdn@gmail.com>
2021-11-14 21:01:48 +0700

    add env variables

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb://afifudin:hIeC2nLWT1VurroJ@cluster0-shard-00-00.jy7lh.mongodb.net:27017,cluster0-shard-00-01.jy7lh.mongodb.net:27017,cluster0-shard-00-02.jy7lh.mongodb.net:27017/restaurant?ssl=true&replicaSet=atlas-tmut6w-shard-0&authSource=admin&retryWrites=true&w=majority",
+    JWT_SECRET: "hIeC2nLWT1VurroJ",
+  },
+};
